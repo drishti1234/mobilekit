a2e9ca89914cbd0d44d65f59574d20ac4514875a
Drishti Samvedi <drishti@farintsol.com>
2022-02-14 18:49:08 +0530

    First Push

--- aspen/settings.py
+++ aspen/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'q4o=#nt2(bbf8ko(0hfuf=_$&r%h-u1np4@jeeo@*8nrv+151)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
